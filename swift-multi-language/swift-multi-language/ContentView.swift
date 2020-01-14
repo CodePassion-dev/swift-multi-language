015ef3f7b56ebf625a68b5cf8e22f16b2e132141
Huy Duong <minhhuy.uit@gmail.com>
2020-01-13 22:40:26 -0600

    Init project

---
+++ swift-multi-language/swift-multi-language/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-multi-language
+//
+//  Created by Huy Duong on 1/13/20.
+//  Copyright © 2020 Huy Duong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
